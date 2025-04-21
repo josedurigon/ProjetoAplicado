75e310d28b15cfaffb6067ec60f021dae778b311
jose durigon <jose.durigon@outlook.com>
2025-04-21 10:19:51 -0300

    Tela login e roteamento

--- src/app/layouts/NonAuthLayout/Components/login/login.component.ts
+++ src/app/layouts/NonAuthLayout/Components/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      const { email, password } = this.form.value;
+      // autenticação vai aqui (serviço JWT, etc)
+      console.log('Login:', { email, password });
+    }
+  }
 }
--- src/app/layouts/NonAuthLayout/non-auth-layout/non-auth-layout.component.ts
+++ src/app/layouts/NonAuthLayout/non-auth-layout/non-auth-layout.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { HomeComponent } from '../Components/home/home.component';
 import { HeaderComponent } from '../Components/header/header.component';
 import { FooterComponent } from '../Components/footer/footer.component';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-non-auth-layout',
   standalone: true,
-  imports: [HomeComponent, HeaderComponent, FooterComponent],
+  imports: [RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './non-auth-layout.component.html',
   styleUrl: './non-auth-layout.component.css'
 })
