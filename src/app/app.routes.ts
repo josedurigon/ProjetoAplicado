3e5d66b775eed68784dd42101b17167fec995815
jose durigon <jose.durigon@outlook.com>
2025-04-21 10:08:09 -0300

    primeiro commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { NonAuthLayoutComponent } from './layouts/NonAuthLayout/non-auth-layout/non-auth-layout.component';
+import { HomeComponent } from './layouts/NonAuthLayout/Components/home/home.component';
+import { LoginComponent } from './layouts/NonAuthLayout/Components/login/login.component';
+import { AuthLayoutComponent } from './layouts/AuthLayout/auth-layout/auth-layout.component';
+import { HomeComponentAuth } from './layouts/AuthLayout/Components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: NonAuthLayoutComponent,
+        children: [
+          { path: '', component: HomeComponent },
+          { path: 'login', component: LoginComponent }
+        ]
+      },
+      {
+        path:'app',
+        component: AuthLayoutComponent,
+        children:[
+            {path: '', component: HomeComponentAuth}
+        ]
+      }
+];
---
+++ src/app/layouts/NonAuthLayout/non-auth-layout/non-auth-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from '../Components/home/home.component';
+import { HeaderComponent } from '../Components/header/header.component';
+import { FooterComponent } from '../Components/footer/footer.component';
+
+@Component({
+  selector: 'app-non-auth-layout',
+  standalone: true,
+  imports: [HomeComponent, HeaderComponent, FooterComponent],
+  templateUrl: './non-auth-layout.component.html',
+  styleUrl: './non-auth-layout.component.css'
+})
+export class NonAuthLayoutComponent {
+
+}
