d1a13e4203f7081ad4a3c6478638f60be951b727
jose durigon <jose.durigon@outlook.com>
2025-05-29 15:19:15 -0300

    jwt validação

--- src/app/layouts/AuthLayout/Components/dashboard/dashboard.component.ts
+++ src/app/layouts/AuthLayout/Components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +8,36 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+
+  ngOnInit(): void {
+    
+  }
+
+
+
+  constructor(private http: HttpClient) {}
+
+ emitirRelatorio() {
+    this.gerarRelatorioVulnerabilidades().subscribe(blob => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'relatorio-vulnerabilidades.pdf';
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    }, err => {
+      console.error('Erro ao baixar relatório', err);
+    });
+  }
+
+
+ gerarRelatorioVulnerabilidades() {
+    return this.http.get('http://localhost:8086/api/relatorios/vulnerabilidades', {
+      responseType: 'blob' // necessário para lidar com o PDF
+    });
+  }
 
 }
