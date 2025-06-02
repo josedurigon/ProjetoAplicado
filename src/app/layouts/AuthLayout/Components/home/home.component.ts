c30f4ee703679600efbdac3848d63cffe4ecc54a
pedrovermude19 <pedrovermude17@gmail.com>
2025-06-02 15:35:20 -0300

    alterações css dashboard3

--- src/app/layouts/AuthLayout/Components/home/home.component.ts
+++ src/app/layouts/AuthLayout/Components/home/home.component.ts
@@ -1,25 +1,26 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { ExploitService } from '../../../../service/exploit.service';
-import { DashboardComponent } from '../dashboard/dashboard.component';
 import { Router } from '@angular/router';
+import { ExploitService } from '../../../../service/exploit.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule, DashboardComponent],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponentAuth {
   url: string = '';
   resultado: any;
-  mostrarDashboard = false;
   isLoading = false;
   errorMessage = '';
 
-  constructor(private exploitService: ExploitService,   private router: Router ) {}
+  constructor(
+    private exploitService: ExploitService,
+    private router: Router
+  ) {}
 
   onSearch() {
     this.isLoading = true;
@@ -29,11 +30,8 @@ export class HomeComponentAuth {
     this.exploitService.exploitHost(body).subscribe({
       next: (res) => {
         this.resultado = res;
-        setTimeout(() => {
-          this.isLoading = false;
-          this.mostrarDashboard = true;
-        }, 1000); // Delay suave para UX
-        this.router.navigate(['dashboard'])
+        this.isLoading = false;
+        this.router.navigate(['/app/dashboard']);
       },
       error: (err) => {
         this.isLoading = false;
