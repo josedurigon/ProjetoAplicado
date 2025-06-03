661316a84d39151f8bed0e9093c9899a0d259bfc
jose durigon <jose.durigon@outlook.com>
2025-06-03 13:56:45 -0300

    Subindo retorno ao cadastrar empresa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,7 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       withInterceptors([authInterceptor])
-    ), provideAnimationsAsync()
+    ), provideAnimationsAsync(), 
     
     ]
 };
--- src/app/layouts/NonAuthLayout/Components/cadastrar-empresa/cadastrar-empresa.component.ts
+++ src/app/layouts/NonAuthLayout/Components/cadastrar-empresa/cadastrar-empresa.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { EmpresaService } from '../../../../service/empresa.service';
+import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-cadastrar-empresa',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule,MatSnackBarModule],
   templateUrl: './cadastrar-empresa.component.html',
   styleUrl: './cadastrar-empresa.component.css'
 })
@@ -18,7 +19,8 @@ export class CadastrarEmpresaComponent {
 
 
 
- constructor(private fb: FormBuilder, private router: Router, private empresaService: EmpresaService) {
+
+ constructor(private fb: FormBuilder, private router: Router, private empresaService: EmpresaService,private snackBar: MatSnackBar) {
     this.form = this.fb.group({
       nome: ['', Validators.required],
       cnpj: ['', [Validators.required, Validators.minLength(14)]],
@@ -37,9 +39,21 @@ export class CadastrarEmpresaComponent {
       this.empresaService.salvarEmpresa(body).subscribe({
         next: (res) =>{
           console.log(res)
+           this.snackBar.open('Empresa cadastrada com sucesso', 'Fechar', {
+            duration: 3000,
+            panelClass: ['snackbar-success'],
+              horizontalPosition: 'center',
+              verticalPosition: 'top'
+          });
         },
         error: (err)=>{
           console.error(err)
+           this.snackBar.open('Erro ao cadastrar empresa', 'Fechar', {
+            duration: 3000,
+            panelClass: ['snackbar-error'],
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
         }
       })
       console.log('Empresa cadastrada:', body);
